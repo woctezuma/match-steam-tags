a2a6f36fa3db0de92c903a271f3cfbf2c88af15d
Wok <woctezuma@users.noreply.github.com>
2019-08-08 19:04:50 +0200

    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(
     install_requires=[
         'steamspypi',
     ],
-    version='0.1.0',
+    version='0.1.1',
     description='Steam Tags on PyPI',
     long_description='Steam Tags: an API to match Steam tags with appIDs, written in Python 3.',
     long_description_content_type='text/x-rst',
     author='Wok',
     author_email='wok@tuta.io',
     url='https://github.com/woctezuma/match-steam-tags',
-    download_url='https://github.com/woctezuma/match-steam-tags/archive/0.1.0.tar.gz',
+    download_url='https://github.com/woctezuma/match-steam-tags/archive/0.1.1.tar.gz',
     keywords=['steam', 'steamtags', 'tags', 'api'],
     classifiers=[
         'Development Status :: 3 - Alpha',
