40c0112798abd1d16acdc89b601867f54190726d
Wok <woctezuma@users.noreply.github.com>
2020-10-28 16:18:47 +0100

    Bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='steamtags',
-    version='0.1.1',
+    version='0.1.2',
     author='Wok',
     author_email='wok@tuta.io',
     description='Steam Tags on PyPI',
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/woctezuma/match-steam-tags',
-    download_url='https://github.com/woctezuma/match-steam-tags/archive/0.1.1.tar.gz',
+    download_url='https://github.com/woctezuma/match-steam-tags/archive/0.1.2.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=[
         'steamspypi',
